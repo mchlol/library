4536cba884c725adbf105c8097ab09c78becf2b0
mchlol <m.andreabr@gmail.com>
2022-03-27 13:30:13 +1100

    add css and config files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './index.html'
+],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+
+  daisyui: {
+    themes: ["cupcake", "dark"],
+  },
+}
