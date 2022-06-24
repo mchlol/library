dff23e8078c30f8b877e1591ab2d56e4b1eee6da
mchlol <m.andreabr@gmail.com>
2022-06-24 22:36:41 +1000

    create modal close function

--- library.js
+++ library.js
@@ -3,6 +3,7 @@ let myLibrary = [];
 const container = document.querySelector('.container');
 const form = document.querySelector('form');
 const addBtn = document.querySelector('#add-button')
+const modal = document.querySelector('#add-book-modal')
 
 let inputTitle = document.querySelector('#title');
 let inputAuthorLast = document.querySelector('#authorLast');
@@ -155,3 +156,10 @@ function sortBy(sortMethod) {
         return console.warn('no valid sort method found');
     }
 }
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
